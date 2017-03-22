06c8773968c72086c2a07d583a82849f2dcf8607
mangel.tercero <mangel.tercero@pcmangeltercero.ayesa.es>
2017-03-22 08:07:27 +0100

    Mapping bbdd mysql

--- grails-app/domain/es/mtercero/Alquiler.groovy
+++ grails-app/domain/es/mtercero/Alquiler.groovy
@@ -14,22 +14,32 @@ class Alquiler {
     Propiedad propiedad
     Inquilino inquilino
     
-    static constraints = {      
+    static constraints = {     
+        id   blank: false
+        inquilino  blank: false
+        propiedad  blank: false
+        fechaInicio blank: false
+        fechaFin  blank: false
+        luzInicio  blank: false
+        luzFin  blank: false
+        aguaInicio  blank: false
+        aguaFin  blank: false
+        precio  blank: false
+        observacion blank: false
     }
     
     static mapping = {
         table 'alquiler'
         version false
         id column: 'id'
+        inquilino column: 'id_Inquilino'
+        propiedad column: 'id_Propiedad'
         fechaInicio column: 'fechaInicio'
         fechaFin column: 'fechaFin'
         luzInicio column: 'luzInicio'
         luzFin column: 'luzFin'
         aguaInicio column: 'aguaInicio'
         aguaFin column: 'aguaFin'
-        precio column: 'precio'
-        inquilino column: 'id_Inquilino'
-        propiedad column: 'id_Propiedad'
-        
+        precio column: 'precio'        
     }
 }
